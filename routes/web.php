5bc24a937ca12657810d719962de6426d1c8c10c
smartguy1986 <arijit.nandi.2008@gmail.com>
2022-07-22 11:09:53 +0530

    Implemented Ajax on the All packages page

--- routes/web.php
+++ routes/web.php
@@ -61,13 +61,13 @@ Route::get('/packages', function (Request $request) {
             <div class="col-lg-4 col-md-6">
                 <div class="package-wrap">
                 <figure class="feature-image">
-                    // <a href="{{ URL::route(\'packages.details\',$post->id) }}">
-                    //     <img src="{{asset(\'images/packages/\'.$post->imageURL)}}" alt="{{ $post->title }}" class="package-image">
-                    // </a>
+                    <a href="/packages.details/'.$post->id.'">
+                        <img src="images/packages/'.$post->imageURL.'" alt="'.$post->title.'" class="package-image">
+                    </a>
                 </figure>
                 <div class="package-price">
                     <h6>
-                        <span>&#8377; {{ number_format($post->price) }}</span> / per person
+                        <span>&#8377; '.number_format($post->price).'</span> / per person
                     </h6>
                 </div>
                 <div class="package-content-wrap">
@@ -75,21 +75,21 @@ Route::get('/packages', function (Request $request) {
                         <ul>
                             <li>
                             <i class="far fa-clock"></i>
-                            {{$post->days}}D/{{$post->nights}}N
+                            '.$post->days.' D/'.$post->nights.' N
                             </li>
                             <li>
                             <i class="fas fa-user-friends"></i>
-                            {{$post->mingroup}}
+                            '.$post->mingroup.'
                             </li>
                             <li>
                             <i class="fas fa-map-marker-alt"></i>
-                            {{$post->name}}
+                            '.$post->name.'
                             </li>
                         </ul>
                     </div>
                     <div class="package-content">
                         <h3>
-                            // <a href="{{ URL::route(\'packages.details\', $post->slug) }}">{{$post->title}}</a>
+                            <a href="/packages.details/'.$post->slug.'">'.$post->title.'</a>
                         </h3>
                         <div class="review-area">
                             <span class="review-text">(25 reviews)</span>
@@ -97,10 +97,10 @@ Route::get('/packages', function (Request $request) {
                             <span style="width: 60%"></span>
                             </div>
                         </div>
-                        <p>{{ Str::limit($post->descriptions, 200) }}</p>
+                        <p>'.substr($post->descriptions, 0, 200).'</p>
                         <div class="btn-wrap">
-                            // <a href="{{ URL::route(\'packages.details\',$post->id) }}" class="button-text width-6">Book Now<i class="fas fa-arrow-right"></i></a>
-                            // <a href="{{ URL::route(\'packages.details\',$post->id) }}" class="button-text width-6">Wish List<i class="far fa-heart"></i></a>
+                            <a href="/packages.details/'.$post->id.'" class="button-text width-6">Book Now<i class="fas fa-arrow-right"></i></a>
+                            <a href="/packages.details/'.$post->id.'" class="button-text width-6">Wish List<i class="far fa-heart"></i></a>
                         </div>
                     </div>
                 </div>
