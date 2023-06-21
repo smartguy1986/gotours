57411316bb20e8f1e073f936ddd5022f46c69cbe
Arijit nandi <arijit.nandi.2008@gmail.com>
2023-06-21 17:23:13 +0530

    21stJune-2023-2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('index');;
     }
 
     /**
@@ -24,13 +26,22 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('home');
-    } 
+        $data['company_banners'] = DB::table('company_banners')->select('*')->where('status', '1')->orderBy('id', 'DESC')->get();
+        $data['company_details'] = DB::table('company_details')->select('*')->get();
+        $data['destinations'] = DB::table('destinations')->select('*')->where([['status', '=', '1'], ['featured', '=', '1']])->orderBy('created_at', 'desc')->skip(0)->take(2)->get();
+        $data['destinations2'] = DB::table('destinations')->select('*')->where([['status', '=', '1'], ['featured', '=', '1']])->orderBy('created_at', 'desc')->skip(2)->take(2)->get();
+        $data['packages'] = DB::table('packages')->select('packages.*', 'destinations.name')->join('destinations', 'destinations.id', '=', 'packages.destination')->where('packages.status', '=', '1')->orderBy('packages.created_at', 'desc')->skip(0)->take(3)->get();
+        $data['packagesoffers'] = DB::table('packages')->select('packages.*', 'destinations.name', 'destinations.slug as dname')->join('destinations', 'destinations.id', '=', 'packages.destination')->where([['packages.status', '=', '1'], ['is_sale', '=', '1']])->orderBy('packages.created_at', 'desc')->skip(0)->take(3)->get();
+        $data['blogs'] = DB::table("blogs")->selectRaw("blogs.*, COUNT('blog_comment.blog_id') AS totcm, users.name")->leftjoin("blog_comment", "blog_comment.blog_id", "=", "blogs.id")->leftjoin('users', 'users.id', '=', 'blogs.author')->where("blogs.status", "=", '2')->groupBy('blogs.id')->orderBy("blogs.id", "desc")->take(3)->get();
+        $data['categories'] = DB::table('package_category')->selectRaw("package_category.*, COUNT('packages.id') AS dest")->join('packages', 'package_category.id', '=', 'packages.category')->where('package_category.status', '=', '1')->groupBy('package_category.id')->skip(0)->take(6)->get();
+
+        return view('home')->with($data);
+    }
 
     public function userHome()
     {
         return view('layouts.user.userhome');
-    } 
+    }
 
     /**
      * Show the application dashboard.
@@ -53,5 +64,4 @@ class HomeController extends Controller
     {
         return view('layouts.manager.managerHome');
     }
-
 }
